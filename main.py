531976491a35d61be9e0e68d6b2e45daac4a0a1b
thesmartcat76 <i.y.hochreiter@gmail.com>
2026-09-19 20:30:57 +0000

    Fix input prompts for better readability in main function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def main():
     print("Welcome to Bakery!")
     print("\n")
-    start = input("Start game?(y/n)")
+    start = input("Start game?(y/n) ")
     bakery_name = ""
     if start == "y":
-        name_bakery(input("What is your bakery's name?"))
+        name_bakery(input("What is your bakery's name? "))
 def name_bakery(nim):
     bakery_name = nim
 
