b03a7754811ccab9252d7be9e402ca6648292492
thesmartcat76 <i.y.hochreiter@gmail.com>
2026-09-19 16:52:59 -0400

    Initialize bakery variables and update main functions
    Added initial variables for bakery game and updated functions to use them.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+# add inital vars
+bakery_name = None
+lvl = 1
+days = 0
+menu = ["Bread"]
 def main():
     print("Welcome to Bakery!")
     print("\n")
     start = input("Start game?(y/n) ")
-    bakery_name = ""
     if start == "y":
         name_bakery(input("What is your bakery's name? "))
-def name_bakery(nim):
-    bakery_name = nim
+        print("\n")
+        view_stats()
 
+def name_bakery(nim):
+    global bakery_name
+    bakery_name = nim
+def view_stats():
+    print(f"Bakery Name: {bakery_name}")
+    print(f"Level: {lvl}")
+    print(f"Days: {days}")
+    print(f"Menu: {menu}")
 
 
 
