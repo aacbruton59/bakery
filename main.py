1caa0e910a6ab5fbc7253bd6e43834e11f2342bf
thesmartcat76 <i.y.hochreiter@gmail.com>
2026-09-20 12:30:06 +0000

    Add money management functions and update view_stats to display financial information

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ bakery_name = None
 lvl = 1
 days = 0
 menu = ["Bread"]
+Money = 100
+income = 0
+expenses = 0
 def main():
     print("Welcome to Bakery!")
     print("\n")
@@ -20,8 +23,21 @@ def view_stats():
     print(f"Level: {lvl}")
     print(f"Days: {days}")
     print(f"Menu: {menu}")
+    print("Money" + calc_money(Money, income , expenses))
+    print(calc_profit(income, expenses))
 
-
-
+def calc_money(mon, inc, exp):
+    finmon = (mon + inc) - exp
+    return "$" + str(finmon)
+def calc_profit(inc, exp):
+    calres = inc - exp
+    if calres > 0:
+        return str(calres) + " money made."
+    elif calres < 0:
+        return str(neg(calres)) + " money lost."
+    else:
+        return "You didn't make any money today."
+def neg(val):
+    return val - val * 2
 # add main func:
 main()
